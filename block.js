a03dbfadc3c51829654c754920c3db6bd1322184
jwandekoken <jwandekoken@gmail.com>
2020-04-20 23:17:24 -0300

    the genesis block - code

--- block.js
+++ block.js
@@ -1,9 +1,17 @@
+const { GENESIS_DATA } = require("./config");
+
 class Block {
   constructor({ timestamp, lastHash, hash, data }) {
     this.timestamp = timestamp;
     this.lastHash = lastHash;
     this.hash = hash;
     this.data = data;
+  }
+
+  static genesis() {
+    // o this é o mesmo que usar "Block"
+    // new Block(GENESIS_DATA)
+    return new this(GENESIS_DATA);
   }
 }
 
