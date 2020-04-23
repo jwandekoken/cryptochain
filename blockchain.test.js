05a9ef1b8d59696864a76bc0792dabb1335ca594
jwandekoken <jwandekoken@gmail.com>
2020-04-23 11:47:57 -0300

    chain validation - tests

--- blockchain.test.js
+++ blockchain.test.js
@@ -2,7 +2,13 @@ const Blockchain = require("./blockchain");
 const Block = require("./block");
 
 describe("Blockchain", () => {
-  const blockchain = new Blockchain();
+  let blockchain;
+
+  // https://jestjs.io/docs/en/api#beforeeachfn-timeout
+  // Runs a function before each of the tests in this file runs.
+  beforeEach(() => {
+    blockchain = new Blockchain();
+  });
 
   it("contains a `chain` Array instance", () => {
     expect(blockchain.chain instanceof Array).toBe(true);
@@ -16,5 +22,44 @@ describe("Blockchain", () => {
     const newData = "foo bar";
     blockchain.addBlock({ data: newData });
     expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(newData);
+  });
+
+  describe("isValidChain()", () => {
+    describe("when the chain does not start with the genesis block", () => {
+      it("returns false", () => {
+        blockchain.chain[0] = { data: "fake-genesis" };
+
+        expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+      });
+    });
+
+    describe("when the chain starts with the genesis block and has multiple blocks", () => {
+      // a function passada para o beforeEach será executada em todos os testes dentro desse describe, assim não temos que repetir o mesmo código diversas vezes
+      beforeEach(() => {
+        blockchain.addBlock({ data: "Bears" });
+        blockchain.addBlock({ data: "Beets" });
+        blockchain.addBlock({ data: "Battlestar Galactica" });
+      });
+
+      describe("and a lastHash reference has changed", () => {
+        it("returns false", () => {
+          blockchain.chain[2].lastHash = "broken-lastHash";
+          expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+        });
+      });
+
+      describe("and the chain contains a block with an invalid field", () => {
+        it("returns false", () => {
+          blockchain.chain[2].data = "some-bad-and-evil-data";
+          expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+        });
+      });
+
+      describe("and the chain does not contain any invalid blocks", () => {
+        it("returns true", () => {
+          expect(Blockchain.isValidChain(blockchain.chain)).toBe(true);
+        });
+      });
+    });
   });
 });
