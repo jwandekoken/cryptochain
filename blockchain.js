4e119536a8841a5a9012fb215bf9015dfab56319
jwandekoken <jwandekoken@gmail.com>
2020-04-27 11:55:52 -0300

    chain validation - code

--- blockchain.js
+++ blockchain.js
@@ -1,4 +1,5 @@
 const Block = require("./block");
+const cryptoHash = require("./crypto-hash");
 
 class Blockchain {
   constructor() {
@@ -12,6 +13,29 @@ class Blockchain {
     });
 
     this.chain.push(newBlock);
+  }
+
+  static isValidChain(chain) {
+    // como no JS dois objects nunca serão iguais, usando o operador "===", mesmo se tiverem as mesmas key-value-pairs, estamos passando para string o object para podermos realizar essa comparação
+    if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
+      return false;
+    }
+
+    // estamos começando no index 1 pois o index 0 já foi validado no código anterior que checa o genesis block
+    for (let i = 1; i < chain.length; i++) {
+      // destructuring the current block
+      const { timestamp, lastHash, hash, data } = chain[i];
+
+      const actualLastHash = chain[i - 1].hash;
+
+      if (lastHash !== actualLastHash) return false;
+
+      const validatedHash = cryptoHash(timestamp, lastHash, data);
+
+      if (hash !== validatedHash) return false;
+    }
+
+    return true;
   }
 }
 
